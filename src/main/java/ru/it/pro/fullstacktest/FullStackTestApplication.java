2a1f03adc7c6619e719474ab0fe21efcbb113751
pdaja <pda.java@gmail.com>
2020-02-14 15:54:49 +0500

    Initial commit

---
+++ src/main/java/ru/it/pro/fullstacktest/FullStackTestApplication.java
@@ -0,0 +1,13 @@
+package ru.it.pro.fullstacktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/it/pro/fullstacktest/FullStackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.it.pro.fullstacktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
