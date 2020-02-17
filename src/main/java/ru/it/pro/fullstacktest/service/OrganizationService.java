4ef0e1a559a747adf461906a248071791360ed6d
pdaja <pda.java@gmail.com>
2020-02-17 15:01:37 +0500

    OrganizationService added.

---
+++ src/main/java/ru/it/pro/fullstacktest/service/OrganizationService.java
@@ -0,0 +1,43 @@
+package ru.it.pro.fullstacktest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.it.pro.fullstacktest.dao.OrganizationRepository;
+import ru.it.pro.fullstacktest.model.Organization;
+
+import java.util.List;
+
+@Service
+public class OrganizationService {
+
+    private OrganizationRepository dao;
+
+    @Autowired
+    public OrganizationService(OrganizationRepository dao) {
+        this.dao = dao;
+    }
+
+    public Organization add(Organization organization) {
+        return dao.add(organization);
+    }
+
+    public List<Organization> findAll() {
+        return dao.findAll();
+    }
+
+    public Organization findById(Integer id) {
+        return dao.findById(id);
+    }
+
+    public Organization findByName(String name) {
+        return dao.findByName(name);
+    }
+
+    public Organization update(Organization organization) {
+        return dao.update(organization);
+    }
+
+    public Organization delete(Integer id) {
+        return dao.delete(id);
+    }
+}
