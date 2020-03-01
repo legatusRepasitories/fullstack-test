4aae0ec6de6770a1fd6752ce8208c61106b237fb
pdaja <pda.java@gmail.com>
2020-03-01 21:00:25 +0500

    Lombok

--- src/main/java/ru/it/pro/fullstacktest/model/Employee.java
+++ src/main/java/ru/it/pro/fullstacktest/model/Employee.java
@@ -1,43 +1,14 @@
 package ru.it.pro.fullstacktest.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Employee {
     private Long id;
     private String name;
     private Long organizationId;
     private Long chiefId;
 
-    public Employee() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getOrganizationId() {
-        return organizationId;
-    }
-
-    public void setOrganizationId(Long organizationId) {
-        this.organizationId = organizationId;
-    }
-
-    public Long getChiefId() {
-        return chiefId;
-    }
-
-    public void setChiefId(Long chiefId) {
-        this.chiefId = chiefId;
-    }
 }
--- src/main/java/ru/it/pro/fullstacktest/model/Organization.java
+++ src/main/java/ru/it/pro/fullstacktest/model/Organization.java
@@ -1,47 +1,15 @@
 package ru.it.pro.fullstacktest.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Organization {
 
     private Long id;
     private String name;
     private Long headOrganizationId;
 
-    public Organization() {
-
-    }
-
-    public Organization(String name, Long headOrganizationId) {
-        this.name = name;
-        this.headOrganizationId = headOrganizationId;
-    }
-
-    public Organization(Long id, String name, Long headOrganizationId) {
-        this.id = id;
-        this.name = name;
-        this.headOrganizationId = headOrganizationId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getHeadOrganizationId() {
-        return headOrganizationId;
-    }
-
-    public void setHeadOrganizationId(Long headOrganizationId) {
-        this.headOrganizationId = headOrganizationId;
-    }
 }
